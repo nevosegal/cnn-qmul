68faa4aef3c2eb2ac57774e7ab55ad987ebd51c2
Nevo Segal <nevosegal@gmail.com>
2016-06-23 15:50:52 +0100

    init

---
+++ cnn.py
@@ -0,0 +1,108 @@
+# Constructing the CNN
+
+from project import initialise_dataset
+import tensorflow as tf
+import numpy as np
+
+
+data = initialise_dataset()
+train_spectros = data['train_spec_mat']
+train_labels = data['train_labels']
+test_spectros = data['test_spec_mat']
+test_labels = data['test_labels']
+valid_spectros = data['valid_spec_mat']
+valid_labels = data['valid_labels']
+num_classes = data['num_classes']
+
+train_one_hot = np.zeros((len(train_labels), num_classes), dtype=np.int32)
+
+# create a one hot encoding for the labels
+for i, one_hot in enumerate(train_one_hot):
+    label_ix = train_labels[i]
+    one_hot[label_ix] = 1
+
+# example plotting spectrograms
+# lr.display.specshow(data['train_spec_mat'][110], x_axis='time', y_axis='mel')
+# plt.title(str(data['train_labels'][110]))
+# plt.show()
+
+# Convolutional Neural Network
+
+# building the graph
+
+# input
+x = tf.placeholder(tf.float32)
+x_tensor = tf.reshape(x, [-1, 128, 130, 1])
+
+# ground-truth output
+y_ = tf.placeholder(tf.float32)
+
+# weights and biases from input to hidden layer 1
+w_conv1 = tf.Variable(tf.truncated_normal([5, 5, 1, 32], stddev=0.1))
+b_conv1 = tf.Variable(tf.constant(0.1, shape=[32]))
+
+# apply the convolution
+conv1 = tf.nn.conv2d(x_tensor, w_conv1, strides=[1, 1, 1, 1], padding="SAME")
+
+# input to ReLU is w*x + b
+h_conv1 = tf.nn.relu(conv1 + b_conv1)
+h_pool1 = tf.nn.max_pool(h_conv1, strides=[1, 2, 2, 1], ksize=[1, 2, 2, 1], padding="SAME")
+
+# hidden layer 2
+w_conv2 = tf.Variable(tf.truncated_normal([5, 5, 32, 64], stddev=0.1))
+b_conv2 = tf.Variable(tf.constant(0.1, shape=[64]))
+
+conv2 = tf.nn.conv2d(h_pool1, w_conv2, strides=[1, 1, 1, 1], padding="SAME")
+
+h_conv2 = tf.nn.relu(conv2 + b_conv2)
+h_pool2 = tf.nn.max_pool(h_conv2, strides=[1, 2, 2, 1], ksize=[1, 2, 2, 1], padding="SAME")
+
+
+# hidden layer 3
+w_conv3 = tf.Variable(tf.truncated_normal([5, 5, 64, 128], stddev=0.1))
+b_conv3 = tf.Variable(tf.constant(0.1, shape=[128]))
+
+conv3 = tf.nn.conv2d(h_pool2, w_conv3, strides=[1, 1, 1, 1], padding="SAME")
+
+h_conv3 = tf.nn.relu(conv3 + b_conv3)
+h_pool3 = tf.nn.max_pool(h_conv3, strides=[1, 2, 2, 1], ksize=[1, 2, 2, 1], padding="SAME")
+
+
+# fully connected layer
+w_fc1 = tf.Variable(tf.truncated_normal([17 * 16 * 128, 1024], stddev=0.1))
+b_fc1 = tf.Variable(tf.constant(0.1, shape=[1024]))
+
+h_pool3_flat = tf.reshape(h_pool3, [-1, 17 * 16 * 128])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool3_flat, w_fc1) + b_fc1)
+
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+w_fc2 = tf.Variable(tf.truncated_normal([1024, 11], stddev=0.1))
+b_fc2 = tf.Variable(tf.constant(0.1, shape=[11]))
+
+# the result
+y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, w_fc2) + b_fc2)
+
+cross_entropy = -tf.reduce_sum(y_*tf.log(tf.clip_by_value(y_conv, 1e-10, 1.0)))
+
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+sess = tf.InteractiveSession()
+
+sess.run(tf.initialize_all_variables())
+
+batch_size = 128
+
+for i in range(len(train_labels)/batch_size):
+    spectro_batch = train_spectros[i*batch_size:(i*batch_size)+batch_size]
+    one_hot_batch = train_one_hot[i*batch_size:(i*batch_size)+batch_size]
+    train_accuracy = accuracy.eval(feed_dict={x: spectro_batch, y_: one_hot_batch, keep_prob: 1.0})
+
+    print('step %d, training accuracy %g' % (i, train_accuracy))
+    train_step.run(feed_dict={x: spectro_batch, y_: one_hot_batch, keep_prob: 0.5})
+
---
+++ project.py
@@ -0,0 +1,235 @@
+# Serialising and importing datasets
+# based on the code from the Tensorflow tutorial
+
+import librosa as lr
+
+import numpy as np
+import os
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
+training_root = 'dataset/IRMAS-TrainingData/'
+testing_root = 'dataset/IRMAS-TestingData/'
+sr = 22050
+file_length = sr * 3
+
+
+def make_arrays(num_rows, audio_length):
+    if num_rows:
+        data = np.ndarray(shape=(num_rows, audio_length), dtype=np.float32)
+        labels = np.ndarray(shape=num_rows, dtype=np.int32)
+    else:
+        data, labels = None, None
+
+    return data, labels
+
+
+def merge_datasets(pickle_files, num_training, num_validation = 0):
+
+    num_classes = len(pickle_files)
+
+    validation_dataset, validation_labels = make_arrays(num_validation, file_length)
+    training_dataset, training_labels = make_arrays(num_training, file_length)
+    vsize_per_class = num_validation // num_classes
+    tsize_per_class = num_training // num_classes
+
+    vstart, tstart = 0, 0
+    vend, tend = vsize_per_class, tsize_per_class
+    end_l = vsize_per_class+tsize_per_class
+
+    for label, pickle_file in enumerate(pickle_files):
+        try:
+            with open(pickle_file, mode='rb') as f:
+                instrument_set = pickle.load(f)
+
+                # shuffling the data
+                np.random.shuffle(instrument_set)
+                if validation_dataset is not None:
+                    # store vsize_per_class number of validation data from a specific instrument class.
+                    validation_instrument = instrument_set[:vsize_per_class, :]
+                    # store this in the merged dataset, in the appropriate location.
+                    validation_dataset[vstart:vend, :] = validation_instrument
+                    validation_labels[vstart:vend] = label
+                    # increment the start and end positions for the next class.
+                    vstart += vsize_per_class
+                    vend += vsize_per_class
+
+                # store the rest in the training examples dataset
+                train_instrument = instrument_set[vsize_per_class:end_l, :]
+                training_dataset[tstart:tend, :] = train_instrument
+                training_labels[tstart:tend] = label
+                # increment the start and end positions for the next class
+                tstart += tsize_per_class
+                tend += tsize_per_class
+        except Exception as e:
+            print("Cannot open file %s: %s" % (pickle_file, e))
+
+    return validation_dataset, validation_labels, training_dataset, training_labels
+
+
+def randomize(dataset, labels):
+    permutation = np.random.permutation(labels.shape[0])
+    shuffled_dataset = dataset[permutation, :]
+    shuffled_labels = labels[permutation]
+
+    return shuffled_dataset, shuffled_labels
+
+# function to compute the mel spectrogram
+def compute_spectrogram(signal):
+    global sr
+    spec = lr.feature.melspectrogram(signal, sr=sr, n_mels=128)
+    # log_spec = lr.logamplitude(spec, ref_power=np.max)
+
+    return spec
+
+
+def generate_spec_mat(dataset, num_spec):
+    spec_mat = np.zeros((num_spec, 128, 130), dtype=np.float32)
+    for i, s in enumerate(dataset):
+        S = compute_spectrogram(s)
+        spec_mat[i, :, :] = S
+    return spec_mat
+
+
+def read_instrument(instrument_folder):
+
+    # list of files in folder
+    file_list = os.listdir(instrument_folder)
+
+    # create dataset list in advance
+    dataset = np.ndarray(shape=(len(file_list), file_length), dtype=np.float32)
+
+    # counter to check how many audio files were read
+    curr_audio = 0
+    for file_name in file_list:
+        full_path = os.path.join(instrument_folder, file_name)
+        try:
+            # read audio data
+            audio_file, sr = lr.load(full_path)
+
+            # if it is not 3 seconds long as expected, raise an error.
+            if len(audio_file) != file_length:
+                raise Exception("Unexpected file shape: %s" % str(len(audio_file)))
+
+            # store in dataset list
+            dataset[curr_audio, :] = audio_file
+            curr_audio += 1
+        except IOError as e:
+            print("Couldn't read:", file_name, ":", e, "- Skipping...")
+
+    print("%s out of %s audio files were successfully read from %s" %
+          (str(curr_audio), str(len(file_list)), instrument_folder))
+
+    # return only the amount of images that were correctly opened
+    return dataset[0:curr_audio, :]
+
+
+# serialize (pickle) the audio dataset
+def pickle_dataset(folders):
+    pickled_folders = []
+
+    # read all files from each folder in the training set, and pickle
+    for f in folders:
+        pickle_name = f+'.pickle'
+        pickled_folders.append(pickle_name)
+
+        # don't pickle of already pickled
+        if os.path.exists(pickle_name):
+            print("Pickle %s already exists" % pickle_name)
+        else:
+            print("Pickling %s" % f)
+            # read all files in a folder
+            dataset = read_instrument(f)
+            try:
+                with open(pickle_name, 'wb') as output_file:
+                    pickle.dump(dataset, output_file, pickle.HIGHEST_PROTOCOL)
+            except Exception as e:
+                print('Unable to save data to', pickle_name, ':', e)
+
+    return pickled_folders
+
+
+def initialise_dataset():
+    pickle_file = 'IRMAS.pickle'
+    # populate array with full path of each instrument folder
+    train_data_folders = [
+        training_root + folder for folder in os.listdir(training_root)
+        if os.path.isdir(os.path.join(training_root, folder))
+    ]
+
+    # populate array with full path of each instrument folder
+    test_data_folders = [
+        testing_root + folder for folder in os.listdir(testing_root)
+        if os.path.isdir(os.path.join(testing_root, folder))
+    ]
+
+    if not os.path.exists(pickle_file):
+
+        # pickle datasets by instrument
+        print("Pickling train datasets")
+        train_datasets = pickle_dataset(train_data_folders)
+        print("Pickling train datasets")
+        test_datasets = pickle_dataset(test_data_folders)
+
+        num_train, num_test, num_valid = 3630, 440, 165
+        # merge them to one big dataset
+        valid_dataset, valid_labels, train_dataset, train_labels = merge_datasets(train_datasets, num_train, num_valid)
+        _, _, test_dataset, test_labels = merge_datasets(test_datasets, num_test)
+
+        # randomize them
+        train_dataset, train_labels = randomize(train_dataset, train_labels)
+        test_dataset, test_labels = randomize(test_dataset, test_labels)
+        valid_dataset, valid_labels = randomize(valid_dataset, valid_labels)
+
+        print('Training:', train_dataset.shape, train_labels.shape)
+        print('Validation:', valid_dataset.shape, valid_labels.shape)
+        print('Testing:', test_dataset.shape, test_labels.shape)
+
+        train_spec_mat = generate_spec_mat(train_dataset, num_train)
+        test_spec_mat = generate_spec_mat(test_dataset, num_test)
+        valid_spec_mat = generate_spec_mat(valid_dataset, num_valid)
+
+        # create a pickle of the merged datasets
+        try:
+            if not os.path.exists(pickle_file):
+                with open(pickle_file, 'wb') as f:
+                    contents = {
+                        'train_spectros': train_spec_mat,
+                        'train_dataset': train_dataset,
+                        'train_labels': train_labels,
+                        'test_spectros': test_spec_mat,
+                        'test_dataset': test_dataset,
+                        'test_labels': test_labels,
+                        'valid_spectros': valid_spec_mat,
+                        'valid_dataset': valid_dataset,
+                        'valid_labels': valid_labels,
+                    }
+
+                    pickle.dump(contents, f, pickle.HIGHEST_PROTOCOL)
+            else:
+                print('%s already exists.. Aborting pickling' % pickle_file)
+
+        except Exception as e:
+            print("Unable to write to file", pickle_file, ":", e)
+            raise
+
+    try:
+        with open(pickle_file, 'rb') as f:
+            all_data = pickle.load(f)
+            contents = {
+                'train_spec_mat': all_data["train_spectros"],
+                'train_labels': all_data["train_labels"],
+                'test_spec_mat': all_data["test_spectros"],
+                'test_labels': all_data["test_labels"],
+                'valid_spec_mat': all_data["valid_spectros"],
+                'valid_labels': all_data["valid_labels"],
+                'num_classes': len(train_data_folders)
+            }
+
+    except Exception as e:
+        print("Unable to read file", pickle_file, ":", e)
+        raise
+
+    return contents
