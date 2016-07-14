bcb61ba6bf34687dae0fe0942f3a7967aa9c3a77
Nevo Segal <nevosegal@gmail.com>
2016-07-14 17:43:59 +0100

    adds another convolutional layer + dropout after each layer

--- model.py
+++ model.py
@@ -12,6 +12,9 @@ class Model():
 		self.input_size = batch_size
 		self.bn = BatchNormalizer(1, 0.001, True)
 
+		# placeholder to store amount of dropout, fed in training.
+		self.keep_prob = tf.placeholder(tf.float32)
+
 		# building the graph
 		# input layer
 		self.x = tf.placeholder(tf.float32)
@@ -22,51 +25,65 @@ class Model():
 		self.y_ = tf.placeholder(tf.float32)
 
 		# weights and biases from input to hidden layer 1
-		self.weights_conv1 = tf.Variable(tf.truncated_normal([3, 3, 1, 32], stddev=0.1))
-		self.bias_conv1 = tf.Variable(tf.constant(0.1, shape=[32]))
+		self.weights_conv1 = tf.Variable(tf.truncated_normal([3, 3, 1, 64], stddev=0.1))
+		self.bias_conv1 = tf.Variable(tf.constant(0.1, shape=[64]))
 
 		# apply the first convolutional layer, moving in steps of 1 in each direction
 		self.conv1 = tf.nn.conv2d(self.x_tensor_normalized, self.weights_conv1, strides=[1, 1, 1, 1], padding="SAME")
 
 		# apply non-linear function - ReLU. input to it is w*x + b. in this case * stands for convolution 
 		# unlike fully connected layers that use multiplication.
-		self.conv1_relu = tf.nn.relu(self.conv1 + self.bias_conv1)
+		self.relu_conv1 = tf.nn.relu(self.conv1 + self.bias_conv1)
+		self.dropout_conv1 = tf.nn.dropout(self.relu_conv1, self.keep_prob)
 
 		# output from hidden layer after applying max-pooling.
-		self.output_conv1 = tf.nn.max_pool(self.conv1_relu, strides=[1, 2, 2, 1], ksize=[1, 2, 2, 1], padding="SAME")
+		self.output_conv1 = tf.nn.max_pool(self.dropout_conv1, strides=[1, 2, 2, 1], ksize=[1, 2, 2, 1], padding="SAME")
 
 
 		# hidden layer 2
-		self.weights_conv2 = tf.Variable(tf.truncated_normal([3, 3, 32, 64], stddev=0.1))
+		self.weights_conv2 = tf.Variable(tf.truncated_normal([3, 3, 64, 64], stddev=0.1))
 		self.bias_conv2 = tf.Variable(tf.constant(0.1, shape=[64]))
 
 		self.conv2 = tf.nn.conv2d(self.output_conv1, self.weights_conv2, strides=[1, 1, 1, 1], padding="SAME")
 
-		self.conv2_relu = tf.nn.relu(self.conv2 + self.bias_conv2)
-		self.output_conv2 = tf.nn.max_pool(self.conv2_relu, strides=[1, 2, 2, 1], ksize=[1, 2, 2, 1], padding="SAME")
+		self.relu_conv2 = tf.nn.relu(self.conv2 + self.bias_conv2)
+		self.dropout_conv2 = tf.nn.dropout(self.relu_conv2, self.keep_prob)
+
+		self.output_conv2 = tf.nn.max_pool(self.dropout_conv2, strides=[1, 2, 2, 1], ksize=[1, 2, 2, 1], padding="SAME")
 
 
 		# hidden layer 3
-		self.weights_conv3 = tf.Variable(tf.truncated_normal([3, 3, 64, 128], stddev=0.1))
-		self.bias_conv3 = tf.Variable(tf.constant(0.1, shape=[128]))
+		self.weights_conv3 = tf.Variable(tf.truncated_normal([3, 3, 64, 64], stddev=0.1))
+		self.bias_conv3 = tf.Variable(tf.constant(0.1, shape=[64]))
 
 		self.conv3 = tf.nn.conv2d(self.output_conv2, self.weights_conv3, strides=[1, 1, 1, 1], padding="SAME")
 
 		self.relu_conv3 = tf.nn.relu(self.conv3 + self.bias_conv3)
-		self.output_conv3 = tf.nn.max_pool(self.relu_conv3, strides=[1, 2, 2, 1], ksize=[1, 2, 2, 1], padding="SAME")
+		self.dropout_conv3 = tf.nn.dropout(self.relu_conv3, self.keep_prob)
+
+		self.output_conv3 = tf.nn.max_pool(self.dropout_conv3, strides=[1, 2, 2, 1], ksize=[1, 2, 2, 1], padding="SAME")
 
 
+		# hidden layer 4
+		self.weights_conv4 = tf.Variable(tf.truncated_normal([3, 3, 64, 64], stddev=0.1))
+		self.bias_conv4 = tf.Variable(tf.constant(0.1, shape=[64]))
+
+		self.conv4 = tf.nn.conv2d(self.output_conv3, self.weights_conv4, strides=[1, 1, 1, 1], padding="SAME")
+
+		self.relu_conv4 = tf.nn.relu(self.conv4 + self.bias_conv4)
+		self.dropout_conv4 = tf.nn.dropout(self.relu_conv4, self.keep_prob)
+
+		self.output_conv4 = tf.nn.max_pool(self.dropout_conv4, strides=[1, 2, 2, 1], ksize=[1, 2, 2, 1], padding="SAME")
+
+		
 		# first fully connected layer
-		self.weights_fc1 = tf.Variable(tf.truncated_normal([16 * 17 * 128, 1024], stddev=0.1))
+		self.weights_fc1 = tf.Variable(tf.truncated_normal([int(8 * 9 * 64), 1024], stddev=0.1))
 		self.bias_fc1 = tf.Variable(tf.constant(0.1, shape=[1024]))
 
-		self.fc1 = tf.reshape(self.output_conv3, [-1, 16 * 17 * 128])
+		self.fc1 = tf.reshape(self.output_conv4, [-1, int(8 * 9 * 64)])
 
 		# output from fully connected layer after multiplication, bias and ReLU
 		self.relu_fc1 = tf.nn.relu(tf.matmul(self.fc1, self.weights_fc1) + self.bias_fc1)
-
-		# placeholder to store amount of dropout, fed in training.
-		self.keep_prob = tf.placeholder(tf.float32)
 
 		# apply dropout
 		self.output_fc1 = tf.nn.dropout(self.relu_fc1, self.keep_prob)
